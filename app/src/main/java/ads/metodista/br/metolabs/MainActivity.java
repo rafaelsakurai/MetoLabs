c5a9cdd5df575973db949c120e33531386790807
Rafael Sakurai <rafasakurai@gmail.com>
2017-03-30 00:36:11 -0300

    Adicionando a tela inicial e reporte de erro

--- app/src/main/java/ads/metodista/br/metolabs/MainActivity.java
+++ app/src/main/java/ads/metodista/br/metolabs/MainActivity.java
@@ -2,12 +2,23 @@ package ads.metodista.br.metolabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lista = (ListView) findViewById(R.id.labs);
+        String[] itens = getResources().getStringArray(R.array.labs); //vetor de strings
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,itens);
+        lista.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
     }
 }
---
+++ app/src/main/java/ads/metodista/br/metolabs/Report.java
@@ -0,0 +1,11 @@
+package ads.metodista.br.metolabs;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by rafaelsakurai on 29/03/17.
+ */
+
+public class Report extends AppCompatActivity{
+
+}
